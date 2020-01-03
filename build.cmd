c5b4bc43acee7da0049075d09fa442b6d26642a7
Damian Hickey <dhickey@gmail.com>
2020-01-03 13:25:14 +0100

    Formatting

--- build.cmd
+++ build.cmd
@@ -5,18 +5,18 @@ docker build ^
  --tag proxykit-httpoverrides-build .
 
 if errorlevel 1 (
-   echo Docker build failed: Exit code is %errorlevel%
-   exit /b %errorlevel%
+  echo Docker build failed: Exit code is %errorlevel%
+  exit /b %errorlevel%
 )
 
 docker run --rm -it --name proxykit-httpoverrides-build ^
-  -v %cd%:/repo ^
-  -w /repo ^
-  -e FEEDZ_PROXYKIT_API_KEY=%FEEDZ_PROXYKIT_API_KEY% ^
-  proxykit-httpoverrides-build ^
-  dotnet run -p build/build.csproj -c Release -- %*
+ -v %cd%:/repo ^
+ -w /repo ^
+ -e FEEDZ_PROXYKIT_API_KEY=%FEEDZ_PROXYKIT_API_KEY% ^
+ proxykit-httpoverrides-build ^
+ dotnet run -p build/build.csproj -c Release -- %*
 
 if errorlevel 1 (
-   echo Docker build failed: Exit code is %errorlevel%
-   exit /b %errorlevel%
+  echo Docker build failed: Exit code is %errorlevel%
+  exit /b %errorlevel%
 )
