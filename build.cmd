2c924cdab9efb6ce50d1d42fc400943c345cc20c
Damian Hickey <dhickey@gmail.com>
2020-08-16 17:33:54 +0200

    Use my docker-ci build image. Pass in GITHUB_RUN_NUMBER environment variable.

--- build.cmd
+++ build.cmd
@@ -1,19 +1,11 @@
 @ECHO OFF
 
-docker build ^
- -f build.Dockerfile ^
- --tag proxykit-httpoverrides-build .
-
-if errorlevel 1 (
-  echo Docker build failed: Exit code is %errorlevel%
-  exit /b %errorlevel%
-)
-
 docker run --rm -it --name proxykit-httpoverrides-build ^
  -v %cd%:/repo ^
  -w /repo ^
  -e FEEDZ_PROXYKIT_API_KEY=%FEEDZ_PROXYKIT_API_KEY% ^
- proxykit-httpoverrides-build ^
+ -e BUILD_NUMBER=%GITHUB_RUN_NUMBER% ^
+ damianh/dotnet-core-lts-sdks:3 ^
  dotnet run -p build/build.csproj -c Release -- %*
 
 if errorlevel 1 (
